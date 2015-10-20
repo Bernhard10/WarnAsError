426ec5bff3c85ccd90a1524aa54600551d1cf8a4
Bernhard Thiel <thiel@tbi.univie.ac.at>
2015-10-20 14:55:02 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='warnaserror',
+    author='Bernhard Thiel'
+    license="MIT",
+    py_modules = ['warnaserror'],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'warnaserror = warnaserror:WarnAsError'
+            ]
+        },
+    )
