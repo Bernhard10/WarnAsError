28b55d082756029cc06a2ad63435cc66ecf44ef1
hugovk <hugovk@users.noreply.github.com>
2016-12-11 21:44:49 +0200

    Fake test to check option can be called with nosetests

---
+++ tests/test_fake.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Fake test
+"""
+from __future__ import print_function, unicode_literals
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class TestFake(unittest.TestCase):
+
+    def test_fake(self):
+        # Just to check the option can be called with
+        # nosetests for different Python versions.
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
